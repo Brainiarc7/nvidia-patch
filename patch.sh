63f0f3e16a4102be38693395482331dfd76fe9f6
Dennis E. Mungai <dmngaie@gmail.com>
2018-04-12 14:01:20 +0300

    Updated library name to libnvidia-encode.so.1

--- patch.sh
+++ patch.sh
@@ -10,7 +10,7 @@ input="$1"
 output="$2"
 replacecmd='s/\x0f\x84\x0b\xfe\xff\xff/\x90\x90\x90\x90\x90\x90/'
 if [ "$#" -ne 2 ]; then
-  2>&1 echo "Usage: $0 <path to original libnvcuvid.so.xxx.yy> <destination for patched livnvcuvid.so.xxx.yy>"
+  2>&1 echo "Usage: $0 <path to original libnvidia-encode.so.xxx.yy> <destination for patched libnvidia-encode.so.xxx.yy>"
   exit 2
 fi
 if [ "x$(realpath "$input")" = "x$(realpath "$output")" ] ; then
