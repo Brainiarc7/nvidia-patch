4915f39fd387733947efc544e902c79739e77e31
Dennis E. Mungai <dmngaie@gmail.com>
2018-04-21 05:59:51 +0300

    Use Keylase's original patch as it works fine.

--- patch.sh
+++ patch.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
-
-command -v realpath >/dev/null 2>&1 || {
-    echo >&2 "This script requires \`realpath\` command but it\`s not installed.";
-    echo >&2 "Please install \`coreutils\` package.";
-    exit 1;
-}
-
-input="$1"
-output="$2"
-replacecmd='s/\x0f\x84\x0b\xfe\xff\xff/\x90\x90\x90\x90\x90\x90/'
-if [ "$#" -ne 2 ]; then
-  2>&1 echo "Usage: $0 <path to original libnvidia-encode.so.xxx.yy> <destination for patched libnvidia-encode.so.xxx.yy>"
-  exit 2
+input=$1
+output=$2
+if [ -z $1 ]; then
+  echo "Require params - example: ./patch.sh <input file> <output file>"
+  exit
 fi
-if [ "x$(realpath "$input")" = "x$(realpath "$output")" ] ; then
-    sed -i.bak -e "$replacecmd" "$input"
-else
-    sed -e "$replacecmd" "$input" > "$output"
-fi
+sed 's/\x85\xC0\x89\xC5\x75\x18/\x29\xC0\x89\xC5\x90\x90/g' $input > $output
